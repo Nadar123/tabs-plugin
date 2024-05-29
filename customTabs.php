e682caa2aaa9639d489334506359796b0fbf3c2b
nadar_rossano <nadar@sogo.co.il>
2024-05-29 09:04:36 +0300

    root setup for plugin

---
+++ customTabs.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  Plugin Name: custom-tabs-plugin
+  Description: Custom tabs Plugin  help you manage and tabs on your WordPress site.
+  Version: 1.0
+  Author: Nadar Rossano
+  Author URI: http://localhost:10003/
+  Text Domain: riskifiedomin
+*/
+
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+// Check ACF functions.
+if( !class_exists('acf') ) {
+  die('Please install and activate Advanced Custom Fields plugin.');
+}
